1c45068e1a700f3716b0955ecf1f97e7f505a1e4
SonDT21 <mr.doantheson@gmail.com>
2023-03-10 18:43:06 +0700

    Commit

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,22 +1,46 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        double number1, number2;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input two real numbers: ");
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            number1 = input.nextDouble();
-            number2 = input.nextDouble();
-            if (number2 - 0.0 == 0.0)
-                throw new Exception("Can not divide by zero");
-            System.out.println("The division of " + number1 + "/" + number2 + " is: " + number1 / number2);
-        } catch (Exception e) {
-            System.out.println(e);
+        // Input the first number
+        double num1 = 0;
+        while (true) {
+            System.out.print("Enter the first number: ");
+            try {
+                num1 = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
         }
-        System.out.println("Done!");
-        input.close();
+
+        // Input the second number
+        double num2 = 0;
+        while (true) {
+            System.out.print("Enter the second number: ");
+            try {
+                num2 = Double.parseDouble(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        // Divide the numbers
+        try {
+            double result = Exercise1.divide(num1, num2);
+            System.out.println("The result of the division is: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static double divide(double num1, double num2) throws IllegalArgumentException {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Invalid division: Division by zero is not allowed.");
+        }
+        return num1 / num2;
     }
 }
