96286c59d008776b4d70888bdaefac7030b12cbf
SonDT21 <mr.doantheson@gmail.com>
2023-03-10 20:09:18 +0700

    Commit

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -3,36 +3,31 @@ import java.util.Scanner;
 public class Exercise1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1, num2, result;
-
-        // Input the first number
-        System.out.print("Enter the first number: ");
-        while (true) {
-            try {
-                num1 = Double.parseDouble(scanner.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.print("Invalid input. Please enter a number: ");
-            }
-        }
-
-        // Input the second number
-        System.out.print("Enter the second number: ");
-        while (true) {
-            try {
-                num2 = Double.parseDouble(scanner.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.print("Invalid input. Please enter a number: ");
-            }
-        }
-
-        // Divide the two numbers
+        double num1, num2;
         try {
-            result = num1 / num2;
-            System.out.println(num1 + " / " + num2 + " = " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Invalid division. The second number cannot be 0.");
+            System.out.print("Enter first number: ");
+            num1 = Double.parseDouble(scanner.nextLine());
+
+            System.out.print("Enter second number: ");
+            num2 = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Result: " + divide(num1, num2));
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
         }
+    }
+
+    public static double divide(double num1, double num2) {
+        double result = 0;
+        try {
+            if (num2 == 0) {
+                throw new ArithmeticException("Cannot divide by zero.");
+            }
+            result = num1 / num2;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        return result;
     }
 }
